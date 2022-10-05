53600947af0af1662c74a93b4f93cc796b4002a6
jiyoon99 <jiyun990422@gmail.com>
2022-10-05 13:45:51 +0900

    AWS 4일 차

--- src/main/java/com/mega/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/mega/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.mega.book.springboot.service.posts;
 
 import com.mega.book.springboot.domain.post.Posts;
 import com.mega.book.springboot.domain.post.PostsRepository;
+import com.mega.book.springboot.web.dto.PostsListResponseDto;
 import com.mega.book.springboot.web.dto.PostsResponseDto;
 import com.mega.book.springboot.web.dto.PostsSaveRequestDto;
 import com.mega.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,5 +30,16 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto :: new)
+                .collect(Collectors.toList());
+    }
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id"+id));
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/mega/book/springboot/web/PostsApiController.java
+++ src/main/java/com/mega/book/springboot/web/PostsApiController.java
@@ -20,4 +20,10 @@ public class PostsApiController {
     }
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){return postsService.findById(id);}
+
+@DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+}
 }
